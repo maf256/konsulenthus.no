8662ef03c075487c93a7ef0402c794c0735086a1
maf256 <maf256@gmail.com>
2021-11-21 03:46:55 +0100

    Fixed Header bug

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,101 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Link from 'next/link'
+import Image from 'next/image'
+import icon from '../public/icon.png'
 
 const Header = () => {
-      return (
-            <div>
-                  <nav>
-                        <ul>
-                              <li>Home</li>
-                              <li>About</li>
+      const [offset, setOffset] = useState(0);
 
+      useEffect(() => {
+            window.onscroll = () => {
+                  setOffset(window.pageYOffset)
+                  console.log(offset);
+            }
+      }, []);
+
+
+
+
+      return (
+
+
+            <Headercontainer YOffset={offset}>
+                  <MenuContainer>
+                        <Image src={icon} alt="Vercel Logo" width={48} height={60} />
+                        <ul>
+                              <li>
+                                    <Link href='/'>Home</Link>
+                              </li>
+                              <li>
+                                    <Link href='/'>Services</Link>
+                              </li>
+                              <li>
+                                    <Link href='/'>Home</Link>
+                              </li>
+                              <li>
+                                    <Link href='/'>Home</Link>
+                              </li>
+                              <li>
+                                    <Link href='/about'>About</Link>
+                              </li>
                         </ul>
-                  </nav>
-            </div>
+                  </MenuContainer>
+            </Headercontainer>
       )
+
 }
 
 export default Header
+
+const Headercontainer = styled.header`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      
+      ${({ YOffset }) => YOffset > 200 ?
+            `height: 4rem; 
+            color: white;
+            background-color: #00000087;
+            box-shadow: 0px 2px 5px rgb(0 0 0 / 10%);
+            `
+            :
+            `height: 7rem; 
+            color: black;
+            background-color: #00000000;
+            box-shadow: 0;
+            `
+      }
+      transition: all .5s ease-in-out;
+
+
+
+
+      /*  */
+      position: fixed;
+      overflow: visible;
+      z-index: 1001;
+      /* background-color: #fff; */
+      
+      
+      ul { 
+            display: flex;
+            justify-content:center;
+            text-align: center;
+            align-items: center;
+            list-style-type: none;
+            li {
+                  padding: 0px 10px;
+            }
+            
+      }
+`
+const MenuContainer = styled.div`
+      display: flex;
+      justify-content: space-between;
+      width: 60%;
+      height: 4rem;
+`
+
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,42 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Header from '../components/header'
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
-export default MyApp
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+        
+      </ThemeProvider>
+    </>
+  )
+}
